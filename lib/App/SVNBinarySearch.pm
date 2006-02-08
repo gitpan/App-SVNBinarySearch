42bfef10cd19fa9e6885b7cf3683346c4e14bac3
Will Coleda <coke@cpan.org>
2006-02-08 22:37:38 +0000

    Import of COKE/App-SVNBinarySearch-v0.1.0 from CPAN.
    gitpan-cpan-distribution: App-SVNBinarySearch
    gitpan-cpan-version:      v0.1.0
    gitpan-cpan-path:         COKE/App-SVNBinarySearch-v0.1.0.tar.gz
    gitpan-cpan-author:       COKE
    gitpan-cpan-maturity:     released

---
+++ lib/App/SVNBinarySearch.pm
@@ -0,0 +1,80 @@
+package App::SVNBinarySearch;
+
+use version; $VERSION = qv('0.01');
+
+use warnings;
+use strict;
+use Carp;
+
+1; # Magic true value required at end of module
+__END__
+
+=head1 NAME
+
+App::SVNBinarySearch - A container for functions in the svn_binary_search
+program
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+No user-serviceable parts inside.  F<svn_binary_search>
+is all that should use this.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+App::SVNBinarySearch requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+Iterator.pm
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-app-svnbinarysearch@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Will Coleda  C<< <coke@cpan.org> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006, Will Coleda C<< <coke@cpan.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'App::SVNBinarySearch' );
+}
+
+diag( "Testing App::SVNBinarySearch $App::SVNBinarySearch::VERSION" );
